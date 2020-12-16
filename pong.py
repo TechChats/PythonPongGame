05f9e3d3927f1a9b8fc8f10679a886d6387f9e54
DonVigu <vigneshmvigi@gmail.com>
2020-12-16 15:38:18 +0530

    added up/down movements to paddles

--- pong.py
+++ pong.py
@@ -44,10 +44,35 @@ ball.penup()
 ball.goto(0,0)
 
 
+# Functions
+def paddle_a_up():
+    y = paddle_a.ycor()
+    y += 20
+    paddle_a.sety(y)
 
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 20
+    paddle_a.sety(y)
 
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y += 20
+    paddle_b.sety(y)
 
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 20
+    paddle_b.sety(y)
 
+# keyboard binding
+wn.listen()
+wn.onkeypress(paddle_a_up,"w") 
+# when we press w key the paddle_a_up functions calls and the left paddle moves up
+wn.onkeypress(paddle_a_down,"s")
+
+wn.onkeypress(paddle_b_up,"Up") 
+wn.onkeypress(paddle_b_down,"Down")
 #Main game loop
 while True:
     wn.update()
